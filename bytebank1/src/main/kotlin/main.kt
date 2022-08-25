a8d92ad6350d56b32ed2ab75d2b10e5b8c4926ac
Marjory <marjory.matos@ibm.com>
2022-08-25 09:49:22 -0300

    implementando varios funcionarios

--- bytebank1/src/main/kotlin/main.kt
+++ bytebank1/src/main/kotlin/main.kt
@@ -1,6 +1,14 @@
 fun main() {
     println("Bem vindo ao Bytebank")
 
+    val alex = Funcionario(
+        nome = "Alex",
+        cpf = "123456789",
+        salario = 1230.0,
+        tipo = 1
+    )
+    println("Nome ${alex.nome} CPF ${alex.cpf} Salario ${alex.salario} ")
+    println("bonificacao ${alex.bonificacao()}")
 }
 
 
